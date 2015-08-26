fef29dc205e9864bb5d31da2dfbccf459bfb923a
skwang <skwang@princeton.edu>
2015-08-25 21:09:16 -0700

    paramterized python script

--- update_champ_data.py
+++ update_champ_data.py
@@ -1,6 +1,9 @@
-import urllib2, json
+import urllib2, json, sys
 
-API_KEY = ''
+if len(sys.argv) != 2:
+	print "usage: python update_champ_data.py [API_KEY]"
+	sys.exit()
+API_KEY = sys.argv[1]
 # get list of champions
 response = urllib2.urlopen('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=' + API_KEY)
 data = json.load(response)
