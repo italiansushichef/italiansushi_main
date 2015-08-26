415a28d3c4c1ee860ef26755ac7a978780a72f88
italiansushichef <AlexZhang@Alexs-MacBook-Pro.local>
2015-08-25 21:25:37 -0700

    new scripts

--- matchid.py
+++ matchid.py
@@ -1,8 +1,9 @@
 from riotwatcher import RiotWatcher, NORTH_AMERICA
 import urllib2, json
 
-w = RiotWatcher('cf473e24-e25e-4402-bdcc-9cb756e90161')
-API_KEY = 'cf473e24-e25e-4402-bdcc-9cb756e90161'
+
+API_KEY = ''
+w = RiotWatcher(API_KEY)
 
 if w.can_make_request() == True:
     yes = urllib2.urlopen('https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/21796238?rankedQueues=RANKED_SOLO_5x5,RANKED_TEAM_5x5&seasons=SEASON2015&api_key=' + API_KEY)
