62bff04d2a04c44bea504374082b4bf70f9f045d
italiansushichef <italinsushichef@gmail.com>
2015-08-26 14:55:13 -0700

    summ id and image change

--- matchid.py
+++ matchid.py
@@ -6,7 +6,7 @@ API_KEY = ''
 w = RiotWatcher(API_KEY)
 
 if w.can_make_request() == True:
-    yes = urllib2.urlopen('https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/21796238?rankedQueues=RANKED_SOLO_5x5,RANKED_TEAM_5x5&seasons=SEASON2015&api_key=' + API_KEY)
+    yes = urllib2.urlopen('https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/21796238?rankedQueues=RANKED_SOLO_5x5&seasons=SEASON2015&api_key=' + API_KEY)
     data = json.load(yes)
     yes.close()
     with open('static/json-data/matchid.json','w') as outfile:
