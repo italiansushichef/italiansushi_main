4b47f12f4f0cb032acd7ecf26ff3ffd61debaf03
skwang <skwang@princeton.edu>
2015-08-31 14:21:38 -0700

    fix url bug

--- italiansushi/urls.py
+++ italiansushi/urls.py
@@ -23,6 +23,6 @@ urlpatterns = patterns('',
         url(r'^login-and-save/$', views.site_login_save, name='login'),
         url(r'^.*\.json$', views.view_itemset, name='view_itemset'),
         url(r'^about/$', views.about_page, name = 'about'),
-        url(r'^faq/$', views.faq_page, name = 'faq')
+        url(r'^faq/$', views.faq_page, name = 'faq'),
         url(r'.*', views.bad_url, name='bad-url'),
         )
